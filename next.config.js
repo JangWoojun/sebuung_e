7a2ff532d3b0f961a735c3e9fb3ca1a29df186c2
JangWoojun <woojun0107@naver.com>
2023-03-29 21:54:41 +0900

    feat: 이미지 관련 설정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["sebuung.s3.ap-northeast-2.amazonaws.com"]
+  }
 }
-
+const withImages = require('next-images')
+module.exports = withImages({
+  webpack(config, options) {
+    return config
+  }
+})
 module.exports = nextConfig
