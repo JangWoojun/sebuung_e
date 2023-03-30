2494a6cf7f3fab9a35e792c3c5c75d7836fbc22d
JangWoojun <woojun0107@naver.com>
2023-03-30 23:41:50 +0900

    feat: 기본 화면 세팅

---
+++ pages/activity.js
@@ -0,0 +1,15 @@
+import Layout from "./components/layout";
+import Head from "next/head";
+
+export default function Activity() {
+    return (
+        <Layout>
+            <Head>
+                <title>세붕이 활동내역</title>
+                <meta name='description' content='세붕이를 소개합니다'/>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+        </Layout>
+      );
+}
+  
---
+++ pages/qna.js
@@ -0,0 +1,15 @@
+import Layout from "./components/layout";
+import Head from "next/head";
+
+export default function QnA() {
+    return (
+        <Layout>
+            <Head>
+                <title>세붕이 QnA</title>
+                <meta name='description' content='세붕이를 소개합니다'/>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+        </Layout>
+      );
+}
+  
