7302dd56faaed037b65391eb8e2ac801153b9ae9
JangWoojun <woojun0107@naver.com>
2023-03-30 23:42:18 +0900

    feat: 페이지 링크 추가

--- pages/components/header.js
+++ pages/components/header.js
@@ -1,21 +1,35 @@
 import Image from 'next/image'
 import logo from '/public/logo2.png'
+import Link from 'next/link';
 
 export default function Header() {
     return (
         <>
             <header className="text-gray-600 body-font">
                 <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-                    <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-                    <Image src={logo} alt="SB logo" width={60} height={60}/>
-                    <span className="ml-3 text-xl">SEBUUNG</span>
-                    </a>
+                    <Link href="/">
+                        <div className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                        <Image src={logo} alt="SB logo" width={60} height={60}/>
+                        <span className="ml-3 text-xl">SEBUUNG</span>
+                        </div>
+                    </Link>
                     <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-                    <a className="mr-5 hover:text-gray-900">홈</a>
-                    <a className="mr-5 hover:text-gray-900">프로젝트</a>
-                    <a className="mr-5 hover:text-gray-900">멤버</a>
-                    <a className="mr-5 hover:text-gray-900">활동 내역</a>
-                    <a className="mr-5 hover:text-gray-900"></a>
+                    <Link href="/">
+                        <div className="mr-5 hover:text-gray-900">홈</div>
+                    </Link>
+
+                    <Link href="project">
+                        <div className="mr-5 hover:text-gray-900">프로젝트</div>
+                    </Link>
+
+                    <Link href="member">
+                        <div className="mr-5 hover:text-gray-900">멤버</div>
+                    </Link>
+
+                    <Link href="activity">
+                        <div className="mr-5 hover:text-gray-900">활동내역</div>
+                    </Link>
+
                     </nav>
                     <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">QnA
                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
