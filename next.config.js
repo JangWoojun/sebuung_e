b2944d8e70fdbb0091cd88d15832cb850cf94eb8
JangWoojun <woojun0107@naver.com>
2023-03-31 22:48:23 +0900

    test: 도메인 추가

--- next.config.js
+++ next.config.js
@@ -2,7 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["sebuung.s3.ap-northeast-2.amazonaws.com"]
+    domains: [
+      "sebuung.s3.ap-northeast-2.amazonaws.com",
+      "yt3.googleusercontent.com",
+      "play-lh.googleusercontent.com",
+      
+  
+  ]
   }
 }
 const withImages = require('next-images')
