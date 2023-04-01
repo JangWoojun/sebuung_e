907651bca7270bd563ef4f6238fe32e6a7c75813
JangWoojun <woojun0107@naver.com>
2023-04-01 20:37:06 +0900

    feat: 고정 헤더 제거

--- pages/components/layout.js
+++ pages/components/layout.js
@@ -4,12 +4,8 @@ import Footer from "./footer"
 export default function Layout({ children }) {
     return (
         <>
-            <div class="relative">
-                <div class="fixed top-0 left-0 w-full h-22 bg-white text-black text-center z-50">
-                    <Header/>
-                </div>
-                <div>{children}</div>
-            </div>
+            <Header/>
+            <div>{children}</div>
             <Footer/>
         </>
     )
