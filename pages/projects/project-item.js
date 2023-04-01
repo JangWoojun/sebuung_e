7a1863291a7be9d9434ad38643b364cc094bc300
JangWoojun <woojun0107@naver.com>
2023-04-01 20:34:31 +0900

    feat: 프로젝트 화면 UI 변경

--- pages/projects/project-item.js
+++ pages/projects/project-item.js
@@ -1,31 +1,47 @@
 import Image from "next/image"
 
-export default function ProjectItem({data}) {
-    
-    const title = data.properties.이름.title[0].plain_text
-    const github = data.properties.Github.url
-    const content = data.properties.설명.rich_text[0].text.content
-    const date = data.properties.기간.number
-    const member = data.properties.팀원.multi_select
-    const image = data.properties.이미지.url
+export default function ProjectItem({ project }) {
+  const title = project.properties.이름.title[0].plain_text
+  const github = project.properties.Github.url
+  const content = project.properties.설명.rich_text[0].text.content
+  const member = project.properties.팀원.multi_select
+  const image = project.properties.이미지.url
+  const type = project.properties.분류.multi_select
 
-    return (
-        <div className="flex flex-col m-3 bg-slate-700 rounded-md rounded-xl w-full
+  return (
+    <a className="bg-white mx-5" href={github}>
+      <div className="flex bg-white rounded-md rounded-xl w-full
         transition duration-300 transform border border-gray-300
-        hover:scale-105 hover: shadow-lg">
-            <a className="rounded-t-xl bg-black" href={github}>
-            <Image alt="프로젝트 이미지" src={image} width={150} height={150} quality={100} layout="responsive" objectFit="cover"/>
-            </a>
-            <div className="p-4 flex flex-col">
-                <h1>{title}</h1>
-                <h3>{content}</h3>
-                <h4>{date}</h4>
-                <div className="flex items-start mt-2">
-                    {member.map((aMember) => (
-                        <h1 className="px-2 py-1 mr-2 rounded-md bg-sky-200 w-30" key={aMember.id}>{aMember.name}</h1>
-                    ))}
-                </div>
-            </div>
+        hover:scale-105 hover:shadow-lg my-5">
+
+        <div className="flex-shrink-0 mr-4 flex justify-center items-center">
+          <Image className="rounded-xl ml-4" alt="프로젝트 이미지" src={image} width={150} height={150} quality={100} />
         </div>
-    )
+
+        <div className="w-full p-4 flex flex-col text-slate-700">
+          <h1 className="text-2xl font-bold">{title}</h1>
+          <h3 className="h-20 text-sm md:text-base lg:text-base break-words mt-2">{content}</h3>
+          <div className="flex items-start mt-2">
+          {type.map((aType) => (
+              <h1
+                className="px-2 py-1 mr-2 rounded-md bg-sky-400 w-20rem font-bold text-white"
+                key={aType.id}
+              >
+                {aType.name}
+              </h1>
+            ))}
+            {member.map((aMember) => (
+              <h1
+                className="px-2 py-1 mr-2 rounded-md bg-sky-500 w-20rem font-bold text-white"
+                key={aMember.id}
+              >
+                {aMember.name}
+              </h1>
+            ))}
+          </div>
+        </div>
+
+      </div>
+    </a>
+  );
 }
