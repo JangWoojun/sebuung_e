afb327e7863f5eb9b5b842519377ab23600713a1
JangWoojun <woojun0107@naver.com>
2023-04-03 17:37:44 +0900

    feat: UX 최적화

--- pages/project.js
+++ pages/project.js
@@ -27,21 +27,21 @@ export default function Project({projects}) {
 
       <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font mb-0">
         <div className="container mx-auto px-5 py-24 md:flex-row flex-col items-start">
-          <h1 className="text-3xl md:text-3xl lg:text-4xl whitespace-pre-line leading-snug font-bold mb-4 text-left ml-6">
+          <h1 className="text-3xl md:text-3xl lg:text-4xl whitespace-pre-line leading-snug font-bold mb-4 text-left ml-4  ">
               {title}<span className="text-sky-500">{tagArr[selectedType]}</span> 프로젝트들을 확인해보세요!
           </h1> 
           <div className="flex justify-start w-full max-w-lg items-start mt-10">
-            <div
+            <button
               className={`flex items-center justify-center w-1/4 h-12 rounded-full border ${
                 selectedType === 3
                   ? 'bg-blue-500 text-white' 
                   : 'border-gray-300 text-gray-500'
-              } mr-4 hover:bg-blue-500 hover:text-white transition-all duration-500 ml-5`}
+              } mr-4 hover:bg-blue-500 hover:text-white transition-all duration-500 ml-4`}
               onClick={() => setSelectedType(3)}
             >
               <h2 className="text-lg font-medium">ALL</h2>
-            </div>
-            <div
+            </button>
+            <button
               className={`flex items-center justify-center w-1/4 h-12 rounded-full border ${
                 selectedType === 0
                   ? 'bg-blue-500 text-white' 
@@ -50,8 +50,8 @@ export default function Project({projects}) {
               onClick={() => setSelectedType(0)}
             >
               <h2 className="text-lg font-medium">iOS</h2>
-            </div>
-            <div
+            </button>
+            <button
               className={`flex items-center justify-center w-1/4 h-12 rounded-full border ${
                 selectedType === 1
                   ? 'bg-blue-500 text-white' 
@@ -60,8 +60,8 @@ export default function Project({projects}) {
               onClick={() => setSelectedType(1)}
             >
               <h2 className="text-lg font-medium">Android</h2>
-            </div>
-            <div
+            </button>
+            <button
               className={`flex items-center justify-center w-1/4 h-12 rounded-full border ${
                 selectedType === 2
                   ? 'bg-blue-500 text-white' 
@@ -70,7 +70,7 @@ export default function Project({projects}) {
               onClick={() => setSelectedType(2)}
             >
               <h2 className="text-lg font-medium">WEB</h2>
-            </div>
+            </button>
           </div>
           <div className="mt-16 grid grid-cols-1 md:grid-cols-2">
             {filteredProjects.map(project => (
