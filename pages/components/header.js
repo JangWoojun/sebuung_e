f18ae5b46bba0887f6e7454f117c8038959a3804
JangWoojun <woojun0107@naver.com>
2023-04-05 19:01:48 +0900

    feat: 제목 강조 및 사용하지 않는 코드 주석

--- pages/components/header.js
+++ pages/components/header.js
@@ -10,7 +10,7 @@ export default function Header() {
                     <Link href="/">
                         <div className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
                         <Image src={logo} alt="SB logo" width={50} height={50}/>
-                        <span className="ml-3 text-xl">SEBUUNG</span>
+                        <span className="ml-3 text-xl text-blue-500 font-extrabold">SEBUUNG</span>
                         </div>
                     </Link>
                     <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
@@ -31,11 +31,11 @@ export default function Header() {
                     </Link>
 
                     </nav>
-                    <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">QnA
+                    {/* <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">QnA
                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
                         <path d="M5 12h14M12 5l7 7-7 7"></path>
                     </svg>
-                    </button>
+                    </button> */}
                 </div>
             </header>
         </>
